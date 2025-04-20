04cbf028bc762be7bbd7112a35718ed7485e4ea6
Maarten Troonbeeckx <maarten@troonbeeckx.net>
2025-04-20 10:44:18 +0200

    Add support for temporary upload URLs

--- tests/StorageDriverTest.php
+++ tests/StorageDriverTest.php
@@ -39,10 +39,33 @@ class StorageDriverTest extends TestCase
         $this->assertStringContainsString('https://account_name.blob.core.windows.net/container/prefix/foobar.txt', $temporaryUrl);
 
         $this->assertMatchesRegularExpression('/sig=(.*)/', $temporaryUrl);
+        $this->assertMatchesRegularExpression('/sp=r/', $temporaryUrl);
 
         $this->assertMatchesRegularExpression(
             sprintf('/se=%sT%sZ/', $expirationDate->format('Y-m-d'), $expirationDate->format('H:i:s')),
             $temporaryUrl
         );
+    }
+
+    public function test_it_can_generate_a_temporary_upload_url()
+    {
+        $expirationDate = now()->addHour();
+
+        $temporaryUpload = Storage::disk('azure_blob_storage_disk')->temporaryUploadUrl('foobar.txt', $expirationDate);
+
+        $this->assertArrayHasKey('url', $temporaryUpload);
+        $this->assertArrayHasKey('headers', $temporaryUpload);
+
+        $this->assertStringContainsString('https://account_name.blob.core.windows.net/container/prefix/foobar.txt', $temporaryUpload['url']);
+
+        $this->assertMatchesRegularExpression('/sig=(.*)/', $temporaryUpload['url']);
+        $this->assertMatchesRegularExpression('/sp=w/', $temporaryUpload['url']);
+
+        $this->assertMatchesRegularExpression(
+            sprintf('/se=%sT%sZ/', $expirationDate->format('Y-m-d'), $expirationDate->format('H:i:s')),
+            $temporaryUpload['url']
+        );
+
+        $this->assertArraySubset(['x-ms-blob-type' => 'BlockBlob'], $temporaryUpload['headers']);
     }
 }
