574d4571d3391b9afccd4c63914b13e1591e4f21
Maarten Troonbeeckx <maarten@troonbeeckx.net>
2025-04-28 15:43:07 +0200

    Add integration test

--- tests/Unit/ServiceProviderTest.php
+++ tests/Unit/ServiceProviderTest.php
@@ -2,9 +2,10 @@
 
 declare(strict_types=1);
 
-namespace Shineability\LaravelAzureBlobStorage\Tests;
+namespace Shineability\LaravelAzureBlobStorage\Tests\Unit;
 
 use GrahamCampbell\TestBenchCore\ServiceProviderTrait;
+use Shineability\LaravelAzureBlobStorage\Tests\TestCase;
 
 class ServiceProviderTest extends TestCase
 {
